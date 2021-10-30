5898397d28e6abdd7e60283c821440287b4cce84
Mario Kahlhofer <mario.kahlhofer@gmail.com>
2021-10-30 15:13:40 +0200

    #2 Let powerberry-app wait in a loop and adapt Dockerfile

---
+++ powerberry-app/src/main.py
@@ -0,0 +1,11 @@
+from time import sleep
+
+
+def main():
+    print("PowerBerry App started and waiting ...", flush=True)
+    while True:
+        sleep(1)
+
+
+if __name__ == "__main__":
+    main()
