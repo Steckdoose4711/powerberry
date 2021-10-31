52cb67496359adcde5b495b5999c4560097392a2
Mario Kahlhofer <mario.kahlhofer@gmail.com>
2021-10-31 20:55:46 +0100

    #9 Read current measurements from Redis for all devices and channels

--- powerberry-app/src/app.py
+++ powerberry-app/src/app.py
@@ -1,6 +1,7 @@
 from time import sleep
 
 import numpy as np
+import numpy.typing as npt
 
 from loguru import logger as log
 
@@ -14,12 +15,44 @@ class App:
         self.cache = Cache(config.redis_host, config.redis_port)
 
     def run(self):
-        log.info("powerberry app started")
-        self.cache.init_synthetic()
-        while True:
-            self.cache.push_synthetic()
-            sleep(1)
+        self.cache.push_synthetic_data()
+        devices = self._query_devices()
 
-    def read_voltages(self) -> np.ndarray:
-        # TODO
-        pass
+        while True:
+            for dev, channels in devices.items():
+                for ch in channels:
+                    self.read_channel(dev, ch)
+            sleep(5)
+
+    def read_channel(self, dev, ch) -> npt.NDArray[np.float32]:
+        sample_rate = self.cache.get_sample_rate(dev, ch)
+        if not sample_rate:
+            log.error(f"no sample rate set for {dev}:{ch}, skip")
+            return
+
+        # wait at least until we have as many samples as the sample rate suggests
+        num_samples = self.cache.get_number_of_samples(dev, ch)
+        if num_samples < sample_rate:
+            log.debug(f"too few samples on {dev}:{ch} yet ({num_samples}), skip")
+            return
+
+        # retrieve full blocks only
+        num_blocks = num_samples // sample_rate
+        arr = self.cache.get_samples(dev, ch, num_blocks * sample_rate)
+        arr = arr.reshape(-1, sample_rate)
+
+        log.info(
+            f"read {num_blocks} blocks (at {sample_rate} Hz each) "
+            f"from {dev}:{ch} "
+            f"(mean values: {arr.mean(axis=1)})"
+        )
+
+    def _query_devices(self):
+        result = {}
+        devices = self.cache.get_devices()
+        for dev in devices:
+            channels = self.cache.get_channels(dev)
+            result[dev] = channels
+
+        log.info(f"queried devices and channels: {result}")
+        return result
