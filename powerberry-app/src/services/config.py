86fb789b781abe26ca3da9718288db77c8217466
Mario Kahlhofer <mario.kahlhofer@gmail.com>
2022-04-13 21:40:53 +0200

    Fix config key prefixes in app

--- powerberry-app/src/services/config.py
+++ powerberry-app/src/services/config.py
@@ -19,10 +19,10 @@ class Config:
         # query variables
         self.from_env("REDIS_HOST", default="localhost")
         self.from_env("REDIS_PORT", default=6379, cast=int)
-        self.from_cfg("influx_host", default="localhost")
-        self.from_cfg("influx_port", default=8086, cast=int)
-        self.from_cfg("voltage_reference", default=228, cast=float)
-        self.from_cfg("sampling_rate", cast=int)
+        self.from_cfg("app_influx_host", default="localhost")
+        self.from_cfg("app_influx_port", default=8086, cast=int)
+        self.from_cfg("app_voltage_reference", default=228, cast=float)
+        self.from_cfg("dsp_sampling_rate", cast=int)
 
     def from_env(self, key, default=None, cast=None):
         value = self._get_key(key, os.environ, "env variable", default, cast)
