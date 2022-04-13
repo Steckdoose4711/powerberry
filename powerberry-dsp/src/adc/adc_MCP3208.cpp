8160e8ab929244d7fbc94c28c1d7b57ec2d0e757
Steckdoose4711 <62461086+Steckdoose4711@users.noreply.github.com>
2022-04-13 21:19:40 +0200

    moved string inside the if

--- powerberry-dsp/src/adc/adc_MCP3208.cpp
+++ powerberry-dsp/src/adc/adc_MCP3208.cpp
@@ -37,9 +37,9 @@ adc_MCP3208::adc_MCP3208(std::shared_ptr<spi_wrapper> const spi, size_t const ch
 
 float adc_MCP3208::read_voltage(size_t const channel)
 {
-    std::string errormessage = "[ERROR]: channel must be between 0 and " + std::to_string(DEFAULT_NUMBER_USED_CHANNELS - 1) + "!";
     if(channel > (DEFAULT_NUMBER_USED_CHANNELS - 1))
     {
+        std::string errormessage = "[ERROR]: channel must be between 0 and " + std::to_string(DEFAULT_NUMBER_USED_CHANNELS - 1) + "!";
         throw errormessage;
     }
     // make them static to do not have to create a new object every time
